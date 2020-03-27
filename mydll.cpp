5aabccd63e20baa93f33b8bcf3b7b44d346c56b2
nonstopfor <zx-zhang18@mails.tsinghua.edu.cn>
2020-03-27 23:46:51 +0800

    modify mydll

--- mydll.cpp
+++ mydll.cpp
@@ -1,28 +1,23 @@
 #include <stdio.h>
 #include "mydll.h"
 #include "definition.h"
-#include <math.h>
 #include <time.h>
-#include <vector>
 #include <utility>
 #include <algorithm>
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
 #define MI 23
 #define NI 19991
-using std::pair;
-using std::make_pair;
-using std::max;
 typedef pair<double, double> PAIR;
-
 /*
 基本的三个添加指令的命令
 info.myCommandList.addCommand(Division,aim_cell_id,direction);//分裂命令，第二个参数是分裂方向
 info.myCommandList.addCommand(Move,aim_cell_id,direction);//移动命令，第二个参数是移动方向
 info.myCommandList.addCommand(Spit,aim_cell_id,direction);//吞吐命令，第二个参数是吞吐方向
 */
-#include<iostream>
-#include<cmath>
-#include<vector>
-using namespace std;
+
 
 double dist(double x1, double y1, double x2, double y2) {
 	double deltaX = x1 - x2;
@@ -71,7 +66,7 @@ bool Judis(PAIR P1, PAIR P2, PAIR yuan, double R) {
 	angle2 = (yuan.first - P2.first) * (P1.first - P2.first) + (yuan.second - P2.second) * (P1.second - P2.second);
 	if (angle1 > 0 && angle2 > 0) return true;//余弦都为正，则是锐角 相交
 	return false;//不相交
-	
+
 }
 bool safe(Info& info, double x1, double y1, double r, double x2, double y2) {
 	TPlayerID myid = info.myID;
@@ -85,16 +80,23 @@ bool safe(Info& info, double x1, double y1, double r, double x2, double y2) {
 		double x = cell.x, y = cell.y;
 		double ar = cell.r;
 		auto p3 = make_pair(x, y);
-		if (Judis(p1, p2, p3, max(0.0, ar - r))) return false;
+		if (Judis(p1, p2, p3, ar + r)) return false;
 	}
 	for (int i = 0; i < info.spikyballInfo.size(); ++i) {
 		auto& t = info.spikyballInfo[i];
 		double x = t.sx, y = t.sy;
 		double ar = t.sr;
 		auto p3 = make_pair(x, y);
-		if (Judis(p1, p2, p3, max(0.0,ar-r))) return false;
+		if (Judis(p1, p2, p3, ar + r)) return false;
 	}
 	return true;
+}
+int compute_dir(double tx, double ty, double sx, double sy) {
+	double dx = tx - sx;
+	double dy = ty - sy;
+	double pi = 3.14159265;
+	int direction = (int)(atan2(dy, dx) / pi * 180 + 360) % 360;
+	return direction;
 }
 
 void player_ai(Info& info)
@@ -109,31 +111,32 @@ void player_ai(Info& info)
 		if (info.cellInfo[i].ownerid == myid)
 			myCell.push_back(info.cellInfo[i]);
 
-	for(int i=0;i<myCell.size();i++)
-		if(myCell[i].r>myCell[maxCell].r)
+	for (int i = 0; i < myCell.size(); i++)
+		if (myCell[i].r > myCell[maxCell].r)
 			maxCell = i;
 
-	for(int i=0;i<myCell.size();i++)
+	for (int i = 0; i < myCell.size(); i++)
 	{
 		int split = splitCheck(myCell, maxCell, i, info.round);
-		int targetX=10000,targetY=10000;
-		if(split!=-1){
+		double targetX = 10000, targetY = 10000;
+		if (split != -1) {
 			targetX = myCell[split].x;
 			targetY = myCell[split].y;
-		}else{
-			for(auto k:info.nutrientInfo){
-				if((targetX-myCell[i].x)*(targetX-myCell[i].x)+(targetY-myCell[i].y)*(targetY-myCell[i].y)<
-					(k.nux-myCell[i].x)*(k.nux-myCell[i].x)+(k.nuy-myCell[i].y)*(k.nuy-myCell[i].y)){
-					if(safe(info,myCell[i].x,myCell[i].y,myCell[i].r,k.nux,k.nuy)){
+		}
+		else {
+			for (auto k : info.nutrientInfo) {
+				if ((targetX - myCell[i].x) * (targetX - myCell[i].x) + (targetY - myCell[i].y) * (targetY - myCell[i].y) >
+					(k.nux - myCell[i].x) * (k.nux - myCell[i].x) + (k.nuy - myCell[i].y) * (k.nuy - myCell[i].y)) {
+					if (safe(info, myCell[i].x, myCell[i].y, myCell[i].r, k.nux, k.nuy)) {
 						targetX = k.nux;
 						targetY = k.nuy;
 					}
 				}
 			}
-			for(auto k:info.cellInfo){
-				if(k.ownerid!=myid && k.r<myCell[i].r && (targetX-myCell[i].x)*(targetX-myCell[i].x)+(targetY-myCell[i].y)*(targetY-myCell[i].y)<
-					(k.x-myCell[i].x)*(k.x-myCell[i].x)+(k.y-myCell[i].y)*(k.y-myCell[i].y)){
-					if(safe(info,myCell[i].x,myCell[i].y,myCell[i].r,k.x,k.y)){
+			for (auto k : info.cellInfo) {
+				if (k.ownerid != myid && k.r < myCell[i].r && (targetX - myCell[i].x) * (targetX - myCell[i].x) + (targetY - myCell[i].y) * (targetY - myCell[i].y) >
+					(k.x - myCell[i].x) * (k.x - myCell[i].x) + (k.y - myCell[i].y) * (k.y - myCell[i].y)) {
+					if (safe(info, myCell[i].x, myCell[i].y, myCell[i].r, k.x, k.y)) {
 						targetX = k.x;
 						targetY = k.y;
 					}
@@ -142,19 +145,16 @@ void player_ai(Info& info)
 		}
 		int direction = 0;
 		double pi = 3.14159265;
-		int dx, dy = 0;
+		//cout << "targetX:" << targetX << " targetY:" << targetY << endl;
+		//cout << "myCellX:" << myCell[i].x << " myCellY:" << myCell[i].y << endl;
 		if (targetX != 10000)
 		{
-			dx = targetX - myCell[i].x;
-			dy = targetY - myCell[i].y;
-			direction = (int)(atan2(dy, dx) / pi * 180 + 360) % 360;
+			direction = compute_dir(targetX,targetY,myCell[i].x,myCell[i].y);
 			info.myCommandList.addCommand(Move, myCell[i].id, direction);
 		}
 		else
 		{
-			dx = 150 - myCell[i].x;
-			dy = 150 - myCell[i].y;
-			direction = (int)(atan2(dy, dx) / pi * 180 + 360) % 360;
+			direction = compute_dir(150, 150, myCell[i].x, myCell[i].y);
 			info.myCommandList.addCommand(Move, myCell[i].id, direction);
 		}
 	}
