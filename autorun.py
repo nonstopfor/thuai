758a07f2931f6e03a25a5e2fe76d5efff33db5d0
kxz-Manjaro <15068701650@163.com>
2020-04-13 23:58:35 +0800

    add best rank

--- autorun.py
+++ autorun.py
@@ -51,9 +51,9 @@ def run(cmd, file_name, times):
                 if (j == 15 and ((pos >= pos_average and pos >= 6) or pos == 1)):
                     with open(directory+f"/round{i}_rank{pos}.txt", 'w') as fout:
                         fout.writelines(lines)
-                    with open(ori_cmd_file, 'r') as cmd_fin:
+                    with open(ori_cmd_file, 'r', encoding='gbk') as cmd_fin:
                         with open(debuglog+f'/round{i}_Original_command.txt', 'w') as cmd_fout:
-                            cmd_fout.write(cmd_fin.read())
+                            cmd_fout.writelines(cmd_fin.readlines())
                 output(f'player {j}: round {i}, ranked {pos}, point {point}', record)
                 output(f'\taverage rank {pos_average}, average point {point_average}, best rank {best_pos}',\
                 record)
