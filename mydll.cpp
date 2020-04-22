fe99756f44fc2ff49b66997b545f33f37b9b0d82
kxz-Manjaro <15068701650@163.com>
2020-04-22 16:36:45 +0800

    add compute time

--- mydll.cpp
+++ mydll.cpp
@@ -128,7 +128,7 @@ double compute_time(CellInfo& cell, double tx, double ty, bool reduce_r = false)
 		(reduce_r ? 2.0 / 3 * cell.r : 0);
 	double acc = 10 / cell.r, top = 20 / cell.r;//加速度，最大速度
 	double direction = (int)(atan2(delta_x, delta_y) / PI * 180 + 360) % 360;
-	double cur_v = cell.v * cos(cell.d - direction);//当前速度
+	double cur_v = cell.v * cos((cell.d - direction) / 180.0 * PI);//当前速度
 	double reach_top_time = (top - cur_v) / acc;//达到最大速度所需时间
 	double reach_top_dist = (top * top - cur_v * cur_v) / 2 / acc;//2ax = vt^2- v0^2
 	double t_consume = 0;
